c3190301a1f7a4b2e766d86d6f8d145d3f5dd762
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2022-07-29 14:17:00 -0500

    please god

--- components/sections/skills/tetris/block.ts
+++ components/sections/skills/tetris/block.ts
@@ -1,9 +1,12 @@
 export class Block {
     image: any;
+    baseImage: any;
     active: boolean;
+    base: boolean;
    
-    constructor(_image) {
+    constructor(_image, _base=false) {
         this.active = true;
         this.image = _image;
+        this.base = _base;
     }
 }
--- components/sections/skills/tetris/matrix.ts
+++ components/sections/skills/tetris/matrix.ts
@@ -6,7 +6,12 @@ export class Matrix {
     cols: number;
     blockSize: number;
     matrix: Array<Array<null | Block>>;
+
     gamma: number;
+    gammaMultiplier: number;
+    gammaBounds: {min: number, max: number};
+    gammaMap: Map<number, number>;
+    // keys: Array<any>;
 
     constructor(_ctx) {
         this.ctx = _ctx;
@@ -31,16 +36,44 @@ export class Matrix {
             this.matrix.push(row);
         }
 
+      
+
+        // Every cell should be worth 2deg of gamma
+        this.gammaMultiplier = 2;
+
+        // Min and max degree bounds of gamma rotation
+        this.gammaBounds = {min: -(this.gammaMultiplier * this.cols / 2), max: this.gammaMultiplier * this.cols / 2};
+
+        let index = 0;
+        this.gammaMap = {} as Map<number, number>;
+        for (let g = this.gammaBounds.min; g <=  this.gammaBounds.max; g+=this.gammaMultiplier) {
+            if (g > 0) {
+                this.gammaMap[`${g}`] = index - 1;
+            } else {
+                this.gammaMap[`${g}`] = index;
+            }
+            
+            index++;
+        }
+        delete this.gammaMap["0"];
+
+        console.log(this.gammaMap)
+
+        // Set gamma
         this.gamma = 0;
         window.addEventListener("deviceorientation", (e)=> {
             if (e.gamma) {
                 this.gamma = Math.round(e.gamma);
 
                 // min and max bounds for left and right movement
-                if (this.gamma < -5) this.gamma = -5;
-                if (this.gamma > 5) this.gamma = 5;
+                if (this.gamma < this.gammaBounds.min) this.gamma = this.gammaBounds.min;
+                if (this.gamma > this.gammaBounds.max) this.gamma = this.gammaBounds.max;
             }
         }, true);
+
+        // this.keys = [];
+        // document.body.addEventListener("keydown", (e) => this.keys[e.key] = true);
+        // document.body.addEventListener("keyup", (e) => this.keys[e.key] = false);
 
         this.drop();
 
@@ -61,81 +94,151 @@ export class Matrix {
 
     }
 
-    move() {
-        // console.log(this.matrix);
+    getIndexFromGamma(_gamma) {
+        if (_gamma < 0) return _gamma / this.gammaMultiplier + 5;
+        return _gamma / this.gammaMultiplier + 4;
+    }
+
+    getGammaFromIndex(_index) {
+        if (_index < 5) return (_index - 5) * this.gammaMultiplier;
+        return (_index - 4) * this.gammaMultiplier;
+    }
+
+    // getDelta() {
+    //     // Iterate over every row
+    //     for (let i = 0; i < this.rows; i++) {
+    //         // Iterate over every column
+    //         for (let j = 0; j < this.cols; j++) {
+    //             // The block is the current active piece
+    //             if (this.matrix[i][j] && this.matrix[i][j].active) {
+    //                 // 2
+    //                 let currentGamma = this.getGammaFromIndex(this.matrix[i][j]);
+
+    //                 // -2
+    //             }
+    //         }
+    //     }
+    // }
+
+    verify(_direction) {
+        let verified = [];
+
+        // Iterate over every row
         for (let i = 0; i < this.rows; i++) {
-            // If the piece is on the left side
-            if (this.gamma < 0) {
-
-                // VERIFY THERE IS ROOM TO MOVE
-
-                // Iterate forwards over each block in the row because we are shifting right to left
-                for (let j = 0; j < this.cols; j++) {
-                    // If the block is the current active piece
-                    if (this.matrix[i][j] && this.matrix[i][j].active) {
-                        // Don't move unless gamma moved by at least 1 in either direction
-                        if (this.gamma + 5 !== j) {
-                            this.matrix[i][this.gamma + 5] = this.matrix[i][j];
-                            this.matrix[i][j] = null;
+            // Iterate over every column
+            for (let j = 0; j < this.cols; j++) {
+                // The block is the current active piece
+                if (this.matrix[i][j] && this.matrix[i][j].active) {
+                    switch(_direction) {
+                        case "down": {
+                            // The block is not on the bottom row
+                            if (i < this.rows - 1) {
+                                // There is a piece at the new position
+                                if (this.matrix[i+1][j]) {
+                                    // The new position is inside of another active piece
+                                    if (this.matrix[i+1][j].active) {
+                                        verified.push(true);
+                                        continue;
+                                    }
+                                } 
+                                // There is no piece at the new position
+                                else {
+                                    verified.push(true);
+                                    continue;
+                                }
+                            } 
+                            verified.push(false);
+                            break;
                         }
-                        
-                    }
-                }
-                
-            } 
-            // If the piece is on the right side
-            else {
 
-                // VERIFY THERE IS ROOM TO MOVE
-
-                // Iterate backwards over each block in the row because we are shifting left to right
-                for (let j = this.cols - 1; j >= 0; j--) {
-                    // If the block is the current active piece
-                    if (this.matrix[i][j] && this.matrix[i][j].active) {
-                        // Don't move unless gamma moved by at least 1 in either direction
-                        if (this.gamma + 4 !== j) {
-                            this.matrix[i][this.gamma + 4] = this.matrix[i][j];
-                            this.matrix[i][j] = null;
+                        case "left": {
+                            // If the block is not on the left most column
+                            if (j > 0) {
+                                // There is a piece at the new position
+                                if (this.matrix[i][j-1]) {
+                                    // The new position is inside of another active piece
+                                    if (this.matrix[i][j-1].active) {
+                                        verified.push(true);
+                                        continue;
+                                    }
+                                } 
+                                // There is no piece at the new position
+                                else {
+                                    verified.push(true);
+                                    continue;
+                                }
+                            }
+                            break;
                         }
-                        
+
+                        case "right": {
+                            // If the block is not on the right most column
+                            if (j < this.cols - 1) {
+                                // There is a piece at the new position
+                                if (this.matrix[i][j+1]) {
+                                    // The new position is inside of another active piece
+                                    if (this.matrix[i][j+1].active) {
+                                        verified.push(true);
+                                        continue;
+                                    }
+                                } 
+                                // There is no piece at the new position
+                                else {
+                                    verified.push(true);
+                                    continue;
+                                }
+                            }
+                            break;
+                        }
+
                     }
+                    
                 }
-                
             }
-            
-            
+        }
+        return !verified.includes(false);
+    }
+
+    move() {
+        if (this.gamma !== 0) {
+            // Iterate over every row
+            for (let i = 0; i < this.rows; i++) { 
+                // Move left
+                if (this.gamma < 0) {
+                    // All active blocks can move left
+                    if (this.verify("left")) {
+                        // Iterate forwards over each column because we are moving left
+                        for (let j = 0; j < this.cols; j++) {
+                            // If the block is the current active piece
+                            if (this.matrix[i][j] && this.matrix[i][j].active) {
+                                this.matrix[i][j - 1] = this.matrix[i][j];
+                                this.matrix[i][j] = null;
+                            }
+                        }
+                    }
+                } 
+                // Move right
+                else {
+                    // All active blocks can move right
+                    if (this.verify("right")) {
+                        // Iterate backwards over each column because we are moving right
+                        for (let j = this.cols - 1; j >= 0; j--) {
+                            // If the block is the current active piece
+                            if (this.matrix[i][j] && this.matrix[i][j].active) {
+                                this.matrix[i][j + 1] = this.matrix[i][j];
+                                this.matrix[i][j] = null;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
     // Handles pieces falling
     drop() {
-        let verified = [];
-
-        // Verify if the active blocks can move down
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; j++) {
-                // If the block is the current active piece
-                if (this.matrix[i][j] && this.matrix[i][j].active) {
-                    // If the block is not on the bottom row
-                    if (i < this.rows - 1) {
-                        if (this.matrix[i+1][j]) {
-                            if (this.matrix[i+1][j].active) {
-                                verified.push(true);
-                                continue;
-                            }
-                        } else {
-                            verified.push(true);
-                            continue;
-                        }
-                    } 
-                    verified.push(false);
-                }
-            }
-            
-        }
-
         // If all active blocks can move down then move down
-        if (!verified.includes(false)) {
+        if (this.verify("down")) {
             for (let i = this.rows - 1; i >= 0; i--) {
                 for (let j = this.cols - 1; j >= 0; j--) {
                     if (this.matrix[i][j] && this.matrix[i][j].active) {
@@ -187,7 +290,7 @@ export class Matrix {
         switch(_name || names[Math.floor(Math.random() * names.length)]) {
             case "OrangeRicky": 
                 this.matrix[0][5] = new Block(image);
-                this.matrix[1][5] = new Block(image);
+                this.matrix[1][5] = new Block(image, true);
                 this.matrix[1][4] = new Block(image);
                 this.matrix[1][3] = new Block(image);
                 break;
@@ -196,20 +299,20 @@ export class Matrix {
                 this.matrix[0][3] = new Block(image);
                 this.matrix[1][3] = new Block(image);
                 this.matrix[1][4] = new Block(image);
-                this.matrix[1][5] = new Block(image);
+                this.matrix[1][5] = new Block(image, true);
                 break;
 
             case "ClevelandZ":
                 this.matrix[0][3] = new Block(image);
                 this.matrix[0][4] = new Block(image);
                 this.matrix[1][4] = new Block(image);
-                this.matrix[1][5] = new Block(image);
+                this.matrix[1][5] = new Block(image, true);
                 break;
 
             case "RhodeIslandZ":
                 this.matrix[0][5] = new Block(image);
                 this.matrix[0][4] = new Block(image);
-                this.matrix[1][4] = new Block(image);
+                this.matrix[1][4] = new Block(image, true);
                 this.matrix[1][3] = new Block(image);
                 break;
 
@@ -217,19 +320,19 @@ export class Matrix {
                 this.matrix[0][3] = new Block(image);
                 this.matrix[0][4] = new Block(image);
                 this.matrix[0][5] = new Block(image);
-                this.matrix[0][6] = new Block(image);
+                this.matrix[0][6] = new Block(image, true);
                 break;
 
             case "Teewee":
                 this.matrix[0][4] = new Block(image);
-                this.matrix[1][4] = new Block(image);
+                this.matrix[1][4] = new Block(image, true);
                 this.matrix[1][3] = new Block(image);
                 this.matrix[1][5] = new Block(image);
                 break;
 
             case "Smashboy": 
                 this.matrix[0][4] = new Block(image);
-                this.matrix[0][5] = new Block(image);
+                this.matrix[0][5] = new Block(image, true);
                 this.matrix[1][4] = new Block(image);
                 this.matrix[1][5] = new Block(image);
                 break;
